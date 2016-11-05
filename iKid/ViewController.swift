60186fdfa2a24cd84e4c37add7460148ab30ddd8
emh <emh>
2016-11-05 00:29:58 -0700

    3 tabs

---
+++ iKid/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by studentuser on 10/31/16.
+//  Copyright © 2016 emh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func backImage(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func backPun(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
